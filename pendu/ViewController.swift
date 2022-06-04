5ce89a7ca643b895ad9df171a803e0d9fe77a938
Philippe Allard-Rousse <philrousse@gmail.com>
2022-06-04 07:17:39 -0400

    Initial Commit

---
+++ pendu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pendu
+//
+//  Created by Philippe Allard-Rousse (Étudiant) on 2022-06-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
